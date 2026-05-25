685a68243e5b97352ecd106c4d3ff93abf163d8c
Suslik <181118v3@gmail.com>
2026-05-25 17:54:43 +0300

    commit

--- exercises/l2_functions/task2.py
+++ exercises/l2_functions/task2.py
@@ -5,7 +5,8 @@
 # by whitespace ' '.
 #
 # For example, call of `join("a", "b")` should return "a b"
-
+def join(str1: str, str2: str) -> str:
+    return f"{str1} {str2}"
 
 # Do not change the below's code
 if __name__ == "__main__":
